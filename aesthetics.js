68ab60f3d818e6be606337514f23c3c241336b5c
Rahmatullaah Boluwatife Ibraheem <ibraheemrahmatullah@gmail.com>
2022-01-17 22:16:59 +0100

    inserts summary

---
+++ aesthetics.js
@@ -0,0 +1,23 @@
+// let elmnt3 = document.getElementById("web");
+
+// function scrollToTopThree() {
+
+//     var testDiv = document.getElementById("web");
+//     if (testDiv.style.display === "none") {
+//       testDiv.style.display = "flex" ;
+//     } else{
+//       testDiv.style.display = "none";
+//     }
+  
+//   elmnt3.scrollIntoView(true); // Top
+// }
+
+
+let catdiv = document.getElementById("collapseTwo");
+
+function toCatDiv(){
+
+
+return catdiv.scrollIntoView(true);
+}
+
